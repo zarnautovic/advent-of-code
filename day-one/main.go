6e55af2a98c84c713eaad5adf95fde0f08b3d919
Zlatan Arnautovic <zlaya.31@gmail.com>
2023-12-01 21:18:46 +0100

    day one advent of code

---
+++ day-one/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+
+func main() {
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath);
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var readLines []string
+
+	for fileScanner.Scan() {
+		readLines = append(readLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+	re := regexp.MustCompile(`[^0-9]+`)
+
+	result1 := part1(readLines, re);
+	result2 := part2(readLines, re);
+
+	fmt.Println("Result of part 1:", result1)
+	fmt.Println("Result of part 2:", result2)
+}
+
+func part1(input []string, re *regexp.Regexp ) int {
+	sum := 0
+	for _, line := range input {
+		spliced := strings.Join(strings.Split(line, ""), " ")
+		numberOnly := re.ReplaceAllString(spliced, "")
+		firstAndLast := string(numberOnly[0]) + string(numberOnly[len(numberOnly)-1])
+		number, err := strconv.Atoi(firstAndLast)
+
+		if (err != nil) {
+			fmt.Println(err)
+		}
+
+		sum += number
+	}
+	return sum
+}
+
+func part2(input []string, re *regexp.Regexp) int {
+	sum := 0
+	for _, line := range input {
+		replaced := applyReplacementRules(line)
+		spliced := strings.Join(strings.Split(replaced, ""), " ")
+		numberOnly := re.ReplaceAllString(spliced, "")
+		firstAndLast := string(numberOnly[0]) + string(numberOnly[len(numberOnly)-1])
+		number, err := strconv.Atoi(firstAndLast)
+
+		if (err != nil) {
+			fmt.Println(err)
+		}
+
+		sum += number
+	}
+	return sum
+}
+	
+func applyReplacementRules(text string) string {
+	replacementRules := map[string]string{
+		"one":   "one1one",
+		"two":   "two2two",
+		"three": "three3three",
+		"four":  "four4four",
+		"five":  "five5five",
+		"six":   "six6six",
+		"seven": "seven7seven",
+		"eight": "eight8eight",
+		"nine":  "nine9nine",
+	}
+
+	for key, value := range replacementRules {
+		text = strings.Replace(text, key, value, -1)
+	}
+
+	return text
+} 
