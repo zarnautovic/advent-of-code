13e6624fa78871d49a4d2e4f8c9842e2329e21d5
Zlatan Arnautovic <zlaya.31@gmail.com>
2023-12-05 08:37:59 +0100

    prepared day 5

---
+++ 05-day-five/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		row := fileScanner.Text();
+
+		fmt.Println(row)
+		// copyOfCards := cards
+	}
+}
