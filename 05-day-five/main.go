667a85f9703e1d63d77cf5932137e4c5bd0a3109
Zlatan <zlaya.31@gmail.com>
2023-12-06 20:05:43 +0100

    day 5 part 2

--- 05-day-five/main.go
+++ 05-day-five/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -15,13 +17,24 @@ func main() {
 	}
 
 	emptyRowCounter := 0
-	var seedsString string
-	var seedSoilMap []string
-	var soilFertilizerMap []string
-	var fertilizerWaterMap []string
-	var waterLightMap []string
-	var lightTemperatureMap []string
-	var temperatureHumidityMap []string
+	var seeds []int
+	var seedSoilMap [][]int
+	var soilFertilizerMap [][]int
+	var fertilizerWaterMap [][]int
+	var waterLightMap [][]int
+	var lightTemperatureMap [][]int
+	var temperatureHumidityMap [][]int
+	var humidityLocationMap [][]int
+
+	var soils []int
+	var fertilizers []int
+	var waters []int
+	var lights []int
+	var temperatures []int
+	var humidities []int
+	var locations []int
+
+	var seedWithRange [][]int
 
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
@@ -33,28 +46,147 @@ func main() {
 			continue
 		}
 		if emptyRowCounter == 0 {
-			seedsString += row
+			seeds = stringToNumbersArray(row)
 		}
 		if emptyRowCounter == 1 {
-			seedSoilMap = append(seedSoilMap, row)
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			seedSoilMap = append(seedSoilMap, array)
 		}
 		if emptyRowCounter == 2 {
-			soilFertilizerMap = append(soilFertilizerMap, row)
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			soilFertilizerMap = append(soilFertilizerMap, array)
 		}
 		if emptyRowCounter == 3 {
-			fertilizerWaterMap = append(fertilizerWaterMap, row)
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			fertilizerWaterMap = append(fertilizerWaterMap, array)
 		}
 		if emptyRowCounter == 4 {
-			waterLightMap = append(waterLightMap, row)
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			waterLightMap = append(waterLightMap, array)
 		}
 		if emptyRowCounter == 5 {
-			lightTemperatureMap = append(lightTemperatureMap, row)
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			lightTemperatureMap = append(lightTemperatureMap, array)
 		}
 		if emptyRowCounter == 6 {
-			temperatureHumidityMap = append(temperatureHumidityMap, row)
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			temperatureHumidityMap = append(temperatureHumidityMap, array)
+		}
+		if emptyRowCounter == 7 {
+			array := stringToNumbersArray(row)
+			if len(array) == 0 {
+				continue
+			}
+			humidityLocationMap = append(humidityLocationMap, array)
 		}
 	}
 
-	fmt.Println(seedsString)
-	fmt.Println(lightTemperatureMap)
+	soils = findInRange(seeds, seedSoilMap)
+	fertilizers = findInRange(soils, soilFertilizerMap)
+	waters = findInRange(fertilizers, fertilizerWaterMap)
+	lights = findInRange(waters, waterLightMap)
+	temperatures = findInRange(lights, lightTemperatureMap)
+	humidities = findInRange(temperatures, temperatureHumidityMap)
+	locations = findInRange(humidities, humidityLocationMap)
+
+	smallestLocation := getSmallestNumberFromArray(locations)
+
+	fmt.Println(smallestLocation)
+
+	for i := 0; i < len(seeds)-1; i = i + 2 {
+		seedWithRange = append(seedWithRange, []int{seeds[i], seeds[i] + seeds[i+1]})
+	}
+
+	i := 0
+	for i < 100000000 {
+		humidity := findInRangeReverse(i, humidityLocationMap)
+		temp := findInRangeReverse(humidity, temperatureHumidityMap)
+		light := findInRangeReverse(temp, lightTemperatureMap)
+		water := findInRangeReverse(light, waterLightMap)
+		fertilizer := findInRangeReverse(water, fertilizerWaterMap)
+		soil := findInRangeReverse(fertilizer, soilFertilizerMap)
+		seed := findInRangeReverse(soil, seedSoilMap)
+		for _, item := range seedWithRange {
+			if seed >= item[0] && seed < item[1] {
+				fmt.Println(i)
+				return
+			}
+		}
+		i++
+	}
+}
+
+func stringToNumbersArray(row string) []int {
+	pattern := `\d+`
+	re := regexp.MustCompile(pattern)
+
+	matches := re.FindAllString(row, -1)
+
+	var numbers []int
+	for _, match := range matches {
+		num, err := strconv.Atoi(match)
+		if err == nil {
+			numbers = append(numbers, num)
+		}
+	}
+
+	return numbers
+}
+
+func findInRange(array []int, mapArray [][]int) []int {
+	var result []int
+	for _, item := range array {
+		for i := 0; i < len(mapArray); i++ {
+			source := mapArray[i][1]
+			r := mapArray[i][2]
+
+			if item >= source && item < source+r {
+				diff := item - source
+				result = append(result, mapArray[i][0]+diff)
+			}
+		}
+	}
+	return result
+}
+
+func findInRangeReverse(number int, mapArray [][]int) int {
+	var result int
+	for i := 0; i < len(mapArray); i++ {
+		source := mapArray[i][0]
+		r := mapArray[i][2]
+
+		if number >= source && number < source+r {
+			diff := number - source
+			result = mapArray[i][1] + diff
+		}
+	}
+	return result
+}
+
+func getSmallestNumberFromArray(array []int) int {
+	smallest := array[0]
+	for _, item := range array {
+		if item < smallest {
+			smallest = item
+		}
+	}
+	return smallest
 }
